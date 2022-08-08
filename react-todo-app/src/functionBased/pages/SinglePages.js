7ce3e26bcd023aa19848b137335852339f0cab46
adiaz-nts <adiaz@neural.com.do>
2022-08-08 14:07:04 -0400

    Trying deploy number 2

--- react-todo-app/src/functionBased/pages/SinglePages.js
+++ react-todo-app/src/functionBased/pages/SinglePages.js
@@ -18,9 +18,12 @@ const SinglePage = () => {
         "This app was developed by Ibas Majid, a self-taught web developer and a technical writer. He is opened to freelance Gig. So go ahead and connect with ibas on Twitter @ibaslogic.",
     },
   ]
+
   const { slug } = useParams()
+
   const aboutContent = aboutData.find(item => item.slug === slug)
   const { title, description } = aboutContent
+
   return (
     <div className="main__content">
       <h1>{title}</h1>
